c1e96734fb988deaf9363999b9945ef8429d02c4
Ui-Adiii <beaheroinyourlife@gmil.com>
2026-07-04 23:39:02 +0530

    changing in connecting in db

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import { TodoProvider } from "@/context/TodoContext";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "My Todo App",
@@ -21,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <TodoProvider>
           <ToastContainer position="top-right" autoClose={3000} />
           {children}
